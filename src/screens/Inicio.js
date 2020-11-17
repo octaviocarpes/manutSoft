1ae564a001160ca28097da5a3d76ff0a4163e3cf
Thiago Nitschke <thiago.nitschke@gmail.com>
2020-11-17 00:10:25 -0300

    Make user deletion work

--- src/screens/Inicio.js
+++ src/screens/Inicio.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import { View, ScrollView, Dimensions, AsyncStorage, Alert, TouchableOpacity } from 'react-native';
-import { auth, db } from '../config';
+import React, {Component} from 'react';
+import {View, ScrollView, Dimensions, AsyncStorage, Alert, TouchableOpacity} from 'react-native';
+import {auth, db} from '../config';
+
 let usuariosRef = db.ref('usuarios/');
 let salasRef = db.ref('salas/');
 import BotaoNovaSala from '../components/BotaoNovaSala';
@@ -12,6 +13,8 @@ import getStatus from '../utils/getStatus';
 import Label from "react-native-material-textfield/src/components/label";
 import BotaoMedio from "../components/BotaoMedio";
 import Icon from "react-native-vector-icons/Ionicons";
+import { StackActions } from 'react-navigation';
+import * as navigation from "react-navigation";
 
 export default class Inicio extends Component {
   constructor(props) {
@@ -21,126 +24,129 @@ export default class Inicio extends Component {
       souAdm: false
     }
   }
-  static navigationOptions = {
+
+  static navigationOptions = ({navigation}) => ({
     title: 'Votações disponíveis',
     headerRight: <TouchableOpacity
-      onPress= {
+      onPress={() => {
         AsyncStorage.getItem('@UID').then((value) => {
-            usuariosRef.child(value).remove()
+          usuariosRef.child(value).remove();
           AsyncStorage.removeItem('@UID');
-            auth.currentUser.delete().then(
-              () => console.log("Usuário deletado!")
-            )
-          })
+          auth.currentUser.delete().then(
+            () => alert("Usuário deletado.")
+          );
+        });
+        navigation.popToTop({immediate: true});
+      }
       }
       style={{paddingRight: 24}}
     >
       <Icon
-      name={'md-trash'}
-      size={24}
-      color="#8400C5"
-    />
+        name={'md-trash'}
+        size={24}
+        color="#8400C5"
+      />
     </TouchableOpacity>,
-  };
+  });
 
   componentWillMount() {
     salasRef.orderByChild("uid").on('value', snapshot => {
       let salas = snapshot.val();
       AsyncStorage.getItem('@UID').then(uid => {
-        if (salas != null) {
-          salas = Object.values(salas);
-          salas = salas.filter(sala => {
-            if(sala.adm_uid === uid){
-              return true;
-            }
-            if(sala.votantes){
-              return [...sala.votantes].filter(v => v.uid === uid).length;
-            }
-          });
-          this.setState(() => ({
-            salas, uid
-          }))
-        }
-      },
-      error => console.log('Erro ao carregar as salas.', error));
+          if (salas != null) {
+            salas = Object.values(salas);
+            salas = salas.filter(sala => {
+              if (sala.adm_uid === uid) {
+                return true;
+              }
+              if (sala.votantes) {
+                return [...sala.votantes].filter(v => v.uid === uid).length;
+              }
+            });
+            this.setState(() => ({
+              salas, uid
+            }))
+          }
+        },
+        error => console.log('Erro ao carregar as salas.', error));
     });
   }
 
   podeVotar = (item, indice) => {
-    const { uid } = this.state;
-    const { questoes } = item;
+    const {uid} = this.state;
+    const {questoes} = item;
     let result = true;
     //se já votou retorna false
     questoes.map((questao, index) => {
-      let { alternativas } = questao;
+      let {alternativas} = questao;
       alternativas.forEach(element => {
-        if(element && element[2]) {
+        if (element && element[2]) {
           result = !(element[2].filter(id => id === uid).length);
           return result;
         }
       });
     });
-    if(result)
+    if (result)
       return true;
-    else 
+    else
       return false;
   }
 
   handleVisualizar = (item, index) => {
-    const { uid, salas } = this.state
-    if (item){
-      if(uid && item.adm_uid === uid){
-        return this.props.navigation.navigate('Andamento', { 'sala': item });
+    const {uid, salas} = this.state
+    if (item) {
+      if (uid && item.adm_uid === uid) {
+        return this.props.navigation.navigate('Andamento', {'sala': item});
       }
-      if(this.podeVotar(item, index))
-        this.props.navigation.navigate('Votacao', { 'sala': item, 'salas': salas, 'indiceSala': index });
-      else 
-      return Alert.alert(
-                '⚠️ Aviso:',
-                'Você já votou nesta sala!',
-                [
-                  {text: 'OK'},
-                ]
-              );
+      if (this.podeVotar(item, index))
+        this.props.navigation.navigate('Votacao', {'sala': item, 'salas': salas, 'indiceSala': index});
+      else
+        return Alert.alert(
+          '⚠️ Aviso:',
+          'Você já votou nesta sala!',
+          [
+            {text: 'OK'},
+          ]
+        );
     } else {
-      this.props.navigation.navigate('Votacao', { 'sala': item, 'salas': salas, 'indiceSala': index });
+      this.props.navigation.navigate('Votacao', {'sala': item, 'salas': salas, 'indiceSala': index});
     }
   }
 
   handleSelect = selected => {
-    this.setState({ selected });
+    this.setState({selected});
   }
 
   getHistorico = salas => salas.filter(item => {
-      const {dataFinal, dataInicial, horaFinal, horaInicial } = item;
-      return getStatus(dataFinal, dataInicial, horaFinal, horaInicial, false) == 'encerrada';
+    const {dataFinal, dataInicial, horaFinal, horaInicial} = item;
+    return getStatus(dataFinal, dataInicial, horaFinal, horaInicial, false) == 'encerrada';
   })
 
   render() {
-    const { salas } = this.state;
-    const { height } = Dimensions.get('screen');
+    const {salas} = this.state;
+    const {height} = Dimensions.get('screen');
     return (
-      <View style={[styles.container, { height: height }]}>
-        <ScrollView style={{ maxHeight: height - 240, marginBottom: 5 }}>
+      <View style={[styles.container, {height: height}]}>
+        <ScrollView style={{maxHeight: height - 240, marginBottom: 5}}>
           <View>
             {
               salas.length > 0 ?
                 salas.map((item, index) =>
-                (getStatus(item.dataFinal,
-                  item.dataInicial, item.horaFinal,
-                  item.horaInicial)) != 'encerrada'?
-                  <CardSalaVotacao
-                    key={index}
-                    onPress={() => this.handleVisualizar(item, index)}
-                    status={getStatus(item.dataFinal,
-                      item.dataInicial, item.horaFinal,
-                      item.horaInicial)}
-                    mensagem={getStatus(item.dataFinal,
-                      item.dataInicial, item.horaFinal,
-                      item.horaInicial, true)}
-                    titulo={item.titulo}
-                  />:
-                  null
+                  (getStatus(item.dataFinal,
+                    item.dataInicial, item.horaFinal,
+                    item.horaInicial)) != 'encerrada' ?
+                    <CardSalaVotacao
+                      key={index}
+                      onPress={() => this.handleVisualizar(item, index)}
+                      status={getStatus(item.dataFinal,
+                        item.dataInicial, item.horaFinal,
+                        item.horaInicial)}
+                      mensagem={getStatus(item.dataFinal,
+                        item.dataInicial, item.horaFinal,
+                        item.horaInicial, true)}
+                      titulo={item.titulo}
+                    /> :
+                    null
                 )
 
                 :
